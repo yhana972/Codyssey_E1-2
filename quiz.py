d8a24d2b429210fe093eb9f413e4dac7c3b0a135
yhana972 <yhana972@gmail.com>
2026-08-08 20:57:53 +0900

    Feat: 퀴즈 힌트 데이터 추가

--- quiz.py
+++ quiz.py
@@ -4,10 +4,11 @@ class Quiz:
     문제 내용, 선택지 목록, 정답 번호, 사용자 답안 판정
     """
 
-    def __init__(self, question: str, choices: list[str], answer: int):
+    def __init__(self, question: str, choices: list[str], answer: int, hint: str = ""):
         self.question = question
         self.choices = choices
         self.answer = answer
+        self.hint = hint
 
     def is_correct(self, user_answer: int) -> bool:
         """사용자 입력 값과 정답을 비교하여 bool 타입으로 반환."""
@@ -22,6 +23,7 @@ class Quiz:
             "question": self.question,
             "choices": self.choices,
             "answer": self.answer,
+            "hint": self.hint,
         }
 
     @classmethod
@@ -30,6 +32,12 @@ class Quiz:
         딕셔너리 데이터를 Quiz 객체로 반환.
         cls : 현재 클래스 자체
         """
+
         return cls(
-            question=data["question"], choices=data["choices"], answer=data["answer"]
+            question=data["question"],
+            choices=data["choices"],
+            answer=data["answer"],
+            hint=data.get(
+                "hint", ""
+            ),  # hint가 있으면 그 값을 가져오고 hint가 없으면 아무것도 반환되지 않음.
         )
